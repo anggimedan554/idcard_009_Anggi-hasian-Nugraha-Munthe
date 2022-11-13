5f8490195046c492f59fae38ac25078ce9ba9177
anggihasian111213medan <anggihasiannugraha051001@gmail.com>
2022-11-13 14:44:11 +0700

    menambahkan template Html beserta membuat konten di index html

--- src/main/java/pws/c/idcardproject/Controlleridcard.java
+++ src/main/java/pws/c/idcardproject/Controlleridcard.java
@@ -10,6 +10,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,16 +21,18 @@ import org.springframework.web.multipart.MultipartFile;
  *
  * @author Lenovo
  */
+@Controller
+
 public class Controlleridcard {
     
     @RequestMapping("/Projectidcard")
-    @ResponseBody
+    
     public String Projectidcard (@RequestParam ("Name") String text,
-                                 @RequestParam ("Tanggal")@DateTimeFormat(pattern = "EEE-yyy-MM-dd")Date date,
+                                 @RequestParam ("Tanggal")@DateTimeFormat(pattern = "yyyy-MM-dd")Date date,
                                  @RequestParam ("image") MultipartFile file, Model model )
             
                                  throws IOException {
-        SimpleDateFormat tanggal = new SimpleDateFormat("EEE-yyy-MM-dd");
+        SimpleDateFormat tanggal = new SimpleDateFormat("EEEE, yyyy-MM-dd");
         
         String newTanggal = tanggal.format(date);
         
