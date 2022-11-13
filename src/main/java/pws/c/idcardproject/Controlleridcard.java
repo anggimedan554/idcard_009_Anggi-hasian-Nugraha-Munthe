d7891488eeaf5689743bc09f36cef2a2c8624e32
anggihasian111213medan <anggihasiannugraha051001@gmail.com>
2022-11-13 11:32:20 +0700

    String date dan string blob

--- src/main/java/pws/c/idcardproject/Controlleridcard.java
+++ src/main/java/pws/c/idcardproject/Controlleridcard.java
@@ -6,9 +6,13 @@
 package pws.c.idcardproject;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -18,7 +22,22 @@ public class Controlleridcard {
     
     @RequestMapping("/Projectidcard")
     public String Projectidcard (@RequestParam ("Name") String text,
-                                 @RequestParam ("Tanggal")@DateTimeFormat(pattern = "yyy-MM-dd")Date date )
+                                 @RequestParam ("Tanggal")@DateTimeFormat(pattern = "EEE-yyy-MM-dd")Date date,
+                                 @RequestParam ("image") MultipartFile file )
+            
                                  throws IOException {
+        SimpleDateFormat tanggal = new SimpleDateFormat("EEE-yyy-MM-dd");
+        
+        String newTanggal = tanggal.format(date);
+        
+        String blob = Base64.encodeBase64String(file.getBytes());
+        
+         return text+"<br><img width=200' src='data:image/jpeg;base64,"+
+               blob+" '/><br>"+
+               newTanggal;
+                        
+    }
+    
+    
     
 }
