0fa43d54f06e138b606f2906a0656b319c49962a
anggihasian111213medan <anggihasiannugraha051001@gmail.com>
2022-11-13 11:59:13 +0700

    membuat index

--- src/main/java/pws/c/idcardproject/Controlleridcard.java
+++ src/main/java/pws/c/idcardproject/Controlleridcard.java
@@ -13,6 +13,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -22,7 +23,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class Controlleridcard {
     
     @RequestMapping("/Projectidcard")
-    
+    @ResponseBody
     public String Projectidcard (@RequestParam ("Name") String text,
                                  @RequestParam ("Tanggal")@DateTimeFormat(pattern = "EEE-yyy-MM-dd")Date date,
                                  @RequestParam ("image") MultipartFile file, Model model )
