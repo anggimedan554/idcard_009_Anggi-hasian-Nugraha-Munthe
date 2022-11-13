3fe6acfc6e4a2f7f49e29bd3fa7a1541974f455d
anggihasian111213medan <anggihasiannugraha051001@gmail.com>
2022-11-13 11:23:00 +0700

    membuat param di controller

--- src/main/java/pws/c/idcardproject/Controlleridcard.java
+++ src/main/java/pws/c/idcardproject/Controlleridcard.java
@@ -5,10 +5,20 @@
  */
 package pws.c.idcardproject;
 
+import java.io.IOException;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  *
  * @author Lenovo
  */
 public class Controlleridcard {
+    
+    @RequestMapping("/Projectidcard")
+    public String Projectidcard (@RequestParam ("Name") String text,
+                                 @RequestParam ("Tanggal")@DateTimeFormat(pattern = "yyy-MM-dd")Date date )
+                                 throws IOException {
     
 }
